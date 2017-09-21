bd370e78815747f46e543c715f80c724f0ff4802
Parth Shah <parth.shah20@gmail.com>
2017-09-21 06:36:57 +0000

    my fist commit

---
+++ webapp/Component.js
@@ -0,0 +1,10 @@
+jQuery.sap.declare("pststtstviewjs.Component");
+sap.ui.getCore().loadLibrary("sap.ui.generic.app");
+jQuery.sap.require("sap.ui.generic.app.AppComponent");
+
+sap.ui.generic.app.AppComponent.extend("pststtstviewjs.Component", {
+	metadata: {
+		"manifest": "json"
+	}
+	//comment
+});
